c018d05fc58f51b254c7661d11204baa9d968f0b
liaojunhao <460200123@qq.com>
2024-06-07 15:50:27 +0800

    删除

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/junhaoliao/Documents/UGit/zhihui-editor/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/junhaoliao/Documents/UGit/zhihui-editor/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/junhaoliao/Documents/UGit/zhihui-editor/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/junhaoliao/Documents/UGit/zhihui-editor/src/css/custom.css"),
-];
